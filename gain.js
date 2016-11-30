e7bf5a16c7a8156cb7f025a7facbd676048bfe80
Nicola Pedretti <np.pedretti@gmail.com>
2016-11-29 18:54:31 -0600

    adding support for multiple feature cuts

--- gain.js
+++ gain.js
@@ -1,25 +1,26 @@
 let _ = require('underscore')
 
-function entropy (elm) {
-	let sum = 0
-	let ident = _.reduce(elm,(found,n) => {
-		found[n] ? found[n] += 1 : found[n] = 1
-		return found
-	} ,{})
-	var entropy = _.reduce(ident, (entr,n) => 
-		entr += -(n/elm.length)*Math.log2(n/elm.length)
-	,0)
-	return entropy
+let gain_calc = {
+
+	entropy: function (elm) {
+		let sum = 0
+		let ident = _.reduce(elm,(found,n) => {
+			found[n] ? found[n] += 1 : found[n] = 1
+			return found
+		} ,{})
+		var entropy = _.reduce(ident, (entr,n) => 
+			entr += -(n/elm.length)*Math.log2(n/elm.length)
+		,0)
+		return entropy
+	}
+
+	gain: function (all, ...args) {
+		let gain = entropy(all)
+		gain -= _.reduce(args, (entr, arg) => {
+			return entr += (arg.length/all.length)*entropy(arg)
+		},0)
+	}	
+
 }
 
-function gain (all, left, right) {
-	return entropy(all) - ((left.length/all.length)*entropy(left) + (right.length/all.length)*entropy(right))
-}
-
-console.log(
-	gain(
-		process.argv[2].split(',').map(Number),
-		process.argv[3].split(',').map(Number),
-		process.argv[4].split(',').map(Number)
-	)
-)
+module.exports = gain_calc
